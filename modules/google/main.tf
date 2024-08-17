ebc354391f03ab922eb5de2a060b8541b79d0ae4
yoshihiro okuda <yoshi.private.acc@gmail.com>
2024-08-18 07:03:53 +0900

    fix: The condition expression must be of type bool.

--- modules/google/main.tf
+++ modules/google/main.tf
@@ -12,7 +12,7 @@ module "google_project_iam_member_service_account" {
   project      = var.project
   for_each     = { for service_account in var.service_accounts : service_account.account_id => service_account }
   account_id   = each.value.account_id
-  display_name = each.value.display_name ? each.value.display_name : each.value.account_id
+  display_name = can(each.value.display_name) ? each.value.display_name : each.value.account_id
   type         = each.value.type
   args         = each.value.args
 }
