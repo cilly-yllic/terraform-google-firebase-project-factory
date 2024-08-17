20f65f5eee03ae964697cf30bde05d98cfb6d8a3
yoshihiro okuda <yoshi.private.acc@gmail.com>
2024-08-18 06:52:36 +0900

    fix: The given for_each argument value is unsuitable.

--- modules/google/main.tf
+++ modules/google/main.tf
@@ -1,7 +1,7 @@
 module "google_project_iam_member_user" {
   source   = "./google_project_iam_member/user"
   project  = var.project
-  for_each = toset(var.users)
+  for_each = { for user in var.users : user.email => user }
   role     = "roles/${each.value.role}"
   member   = "user:${each.value.email}"
   deploy   = each.value.deploy
@@ -10,7 +10,7 @@ module "google_project_iam_member_user" {
 module "google_project_iam_member_service_account" {
   source       = "./google_project_iam_member/service_account"
   project      = var.project
-  for_each     = toset(var.service_accounts)
+  for_each     = { for service_account in var.service_accounts : service_account.account_id => service_account }
   account_id   = each.value.account_id
   display_name = each.value.display_name ? each.value.display_name : each.value.account_id
   type         = each.value.type
